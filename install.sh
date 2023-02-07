ce72dbdf1c133bb46177ea5cce8a8ebf26597058
Alejandro García <alejandrogarcia@Alejandros-MacBook.local>
2023-02-06 18:34:55 -0600

    THEME OMZ

--- install.sh
+++ install.sh
@@ -27,7 +27,8 @@ zshrc() {
 sudo ln -fs /usr/share/zoneinfo/America/Monterrey /etc/localtime
 sudo dpkg-reconfigure --frontend noninteractive tzdata
 
-zsh
+zshrc
+exec zsh
 
 # make directly highlighting readable - needs to be after zshrc line
 echo "" >> ~/.zshrc
